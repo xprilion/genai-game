982c7191f6c8e2ab6dcc62ae8027ffc1449e1a83
xprilion <xprilion@gmail.com>
2023-12-09 10:53:00 +0530

    fix

--- main.py
+++ main.py
@@ -25,15 +25,13 @@ model = TextGenerationModel.from_pretrained("text-bison@001")
 def hello_world():
     return render_template("chat.html")
 
-
-@app.route('/pico', methods=['GET'])
-def pico_page():
-    return render_template("pico.html")
-
 @app.route('/chat/<guess>/<actual>', methods=['GET'])
 def chat(guess, actual):
     response = model.predict(
-        "You are a bot in a guessing game where the player tries to guess a secret item you are thinking about. The player has just guessed " + guess + ". If the user is right, tell them they are right and ask them to refresh the page to start a new game. If they are not very close to the actual item, respond with a humorous remark about their guess, related somehow to their guess and the actual secret item, which is " + actual + ". Then provide a subtle hint to guide the player closer to the actual secret item. Never in your response include the actual item name unless the user has guessed it correctly is is very very close!",
+        "You are the bot in a guessing game where the player tries to guess a secret item you are thinking about. " +
+        "The rules of the game are: 1. the player will make a guess. 2. if the guess is correct or very close to the correct answer, you will say, 'Congratulations, you've got it right!' " +
+         "3. If they are not very close to the actual item, respond with a humorous remark about their guess. Then without mentioning the actual item, " + actual + ", provide a subtle hint to guide the player closer to the secret item." +
+        "The player has just guessed " + guess + ".",
         **parameters
     )
 
