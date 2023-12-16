5a95715b49e0d0897632afbcac8ddfbea5557b36
xprilion <xprilion@gmail.com>
2023-12-16 11:57:26 +0530

    Multimodal Gemini Pro

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ genai.configure(api_key=os.getenv("API_KEY"))
 app = Flask(__name__)
 app.debug = True
 
-
 config = {
   'temperature': 0,
   'top_k': 20,
@@ -39,50 +38,45 @@ safety_settings = [
   }
 ]
 
-model = genai.GenerativeModel(model_name="gemini-pro",
+model = genai.GenerativeModel(model_name="gemini-pro-vision",
                               generation_config=config,
                               safety_settings=safety_settings)
+
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return render_template("chat.html")
 
-@app.route('/chat/<guess>/<actual>', methods=['GET'])
-def chat(guess, actual):
+@app.route('/chat', methods=['POST'])
+def chat():
+    if 'user_image' not in request.files:
+        return jsonify({"error": "No file part"})
 
-    context = "You are the bot in a guessing game where the player tries to guess a secret item you are thinking about. " + \
-        "The rules of the game are: " + \
-        "1. the player will make a guess. " + \
-        "2. if the guess is correct or very close to the correct answer, you will say, 'Congratulations, you've got it right!' " + \
-        "3. If they are not very close to the actual item, respond with a humorous remark about their guess. Then without mentioning the actual item, " + actual + ", provide exactly one hint to guide the player closer to the secret item. Don't repeat player's input. Don't give many hints at once."
-    
-    examples = [
-        [
-            "actual_item: cat. player_input: is it green",
-            "They are mostly never green but are often found in black, white and mixed colors."
-        ],
-        [
-            "actual_item: cat. player_input: a car?",
-            "Instead of four wheels it has four legs!"
-        ],
-        [
-            "actual_item: cat. player_input: a dog?",
-            "You're very close! Yes it is a pet, but not a dog!"
-        ],
-        [
-            "actual_item: cat. player_input: is it a cat?",
-            "Congratulations, yes it is a cat!"
+    file = request.files['user_image']
+
+    if file.filename == '':
+        return jsonify({"error": "No selected file"})
+
+    if file:
+        image_data = file.read()
+        image_parts = [
+            {
+                "mime_type": file.content_type,
+                "data": image_data
+            },
         ]
-    ]
 
-    prompt = "actual_input: " + actual + ". player_input: " + guess
+        prompt_parts = [
+            "You are Sheldon Cooper. User will upload an image. Based on the image, you have to come up with a Sheldon Cooper style fun fact. Also give a funny, sarcastic note about the image. \n\nUser's image:\n\n",
+            image_parts[0],
+            "\n\nFun fact:\n",
+        ]    
 
-    response = model.generate_content(context + "\n\nExamples: " + json.dumps(examples) + "\n\n" + prompt)
+        response = model.generate_content(prompt_parts)
 
-    return jsonify({
-        "response": marko.convert(response.text)
-    })
-
+        return jsonify({
+            "response": marko.convert(response.text)
+        })
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
